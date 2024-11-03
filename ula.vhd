3cd00570f3cb288ad2e1ce7d868dc553a07f0236
Conke94 <gabrielmoroconke@hotmail.com>
2024-11-03 15:06:55 -0300

    feat: set ula to new file and add zero flag

---
+++ ula.vhd
@@ -0,0 +1,26 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ula is
+    port (   
+        x,y : in unsigned(15 downto 0);
+        out_a :  out unsigned(15 downto 0);
+        select_operation_a, select_operation_b : in std_logic;
+        flag_zero: out std_logic
+    );  
+ end entity;
+
+ architecture a_ula of ula is
+    signal result : unsigned(15 downto 0);
+    begin
+        result <= x+y when select_operation_a = '0' and select_operation_b = '0' else
+                  x-y when select_operation_a = '0' and select_operation_b = '1';
+
+        out_a <= result;
+
+        flag_zero <= '1' when result = "0000000000000000" else 
+                     '0';
+ end architecture;
+
+-- DUAS OPERAÇÕES, TESTBENCH
---
+++ ula_tb.vhd
@@ -0,0 +1,76 @@
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+
+entity ula_tb is
+end entity;
+
+architecture a_ula_tb of ula_tb is
+    component ula
+        port(   
+            x,y       :  in  unsigned(15 downto 0);
+            out_a :  out unsigned(15 downto 0);
+            select_operation_a, select_operation_b : in std_logic;
+            flag_zero: out std_logic
+        );
+    end component;
+
+    signal x, y, out_a : unsigned(15 downto 0);
+    signal select_operation_a, select_operation_b, flag_zero : std_logic;
+
+    begin
+        uut : ula port map(
+            x => x,
+            y => y,
+            out_a => out_a,
+            select_operation_a => select_operation_a,
+            select_operation_b => select_operation_b,
+            flag_zero => flag_zero
+        );
+    
+        process
+            begin
+                x <= "1111111111111111";
+                y <= "1111111111111111";
+                select_operation_a <= '0';
+                select_operation_b <= '0';
+                wait for 50 ns;
+                x <= "1111111111111111";
+                y <= "1111111111111111";
+                select_operation_a <= '0';
+                select_operation_b <= '1';
+                wait for 50 ns;
+                x <= "0000000000000000";
+                y <= "0000000000000000";
+                select_operation_a <= '0';
+                select_operation_b <= '0';
+                wait for 50 ns;
+                x <= "0000000000000000";
+                y <= "0000000000000000";
+                select_operation_a <= '0';
+                select_operation_b <= '1';
+                wait for 50 ns;
+                x <= "0000000000001000";
+                y <= "0000000000000100";
+                select_operation_a <= '0';
+                select_operation_b <= '0';
+                wait for 50 ns;
+                x <= "0000000000001000";
+                y <= "0000000000000100";
+                select_operation_a <= '0';
+                select_operation_b <= '1';
+                wait for 50 ns;
+                x <= "0000000000000100";
+                y <= "0000000000001000";
+                select_operation_a <= '0';
+                select_operation_b <= '0';
+                wait for 50 ns;
+                x <= "0000000000000100";
+                y <= "0000000000001000";
+                select_operation_a <= '0';
+                select_operation_b <= '1';
+                wait for 50 ns;
+                wait;
+    end process;
+
+end architecture;
