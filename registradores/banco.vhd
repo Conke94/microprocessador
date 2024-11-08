9048787fc4a1050194488a14c0b9c7eeb03f1f98
Conke94 <gabrielmoroconke@hotmail.com>
2024-11-08 20:15:29 -0300

    feat: banco de registradores

--- registradores/banco.vhd
+++ registradores/banco.vhd
@@ -2,73 +2,45 @@ library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
-entity reg16bits_tb is
+entity banco is
+    port(
+        clk, reset, wr_en : in std_logic;
+        reg_wr: in unsigned(2 downto 0);
+        reg_read: in unsigned(2 downto 0);
+        data_in : in unsigned(15 downto 0);
+        data_out : out unsigned(15 downto 0)
+    );
 end entity;
 
-architecture a_reg16bits_tb of reg16bits_tb is 
-    component reg16bits
+architecture a_banco of banco is 
+    component reg16bits is
         port(
-            clk : in std_logic;
-            reset : in std_logic;
-            wr_en: in std_logic;
+            clk, reset, wr_en : in std_logic;
             data_in : in unsigned(15 downto 0);
             data_out : out unsigned(15 downto 0)
         );
     end component;
-
-    constant period : time := 100 ns;
-    signal finished : std_logic := '0';
-    signal data_in, data_out : unsigned(15 downto 0);
-    signal clk, reset, wr_en : std_logic;
-
+    signal registro: unsigned(15 downto 0);
+    SIGNAL wr_en0, wr_en1, wr_en2, wr_en3, wr_en4 : std_logic := '0';
+    SIGNAL data0, data1, data2, data3, data4 : unsigned(15 DOWNTO 0) := "0000000000000000";
     begin
-        uut: reg16bits port map (
-            data_in => data_in, data_out => data_out,
-            clk => clk, reset => reset,
-            wr_en => wr_en
-        );
+        reg0 : reg16bits PORT MAP(clk => clk, reset => reset, wr_en => wr_en0, data_in => data_in, data_out => data0);
+        reg1 : reg16bits PORT MAP(clk => clk, reset => reset, wr_en => wr_en1, data_in => data_in, data_out => data1);
+        reg2 : reg16bits PORT MAP(clk => clk, reset => reset, wr_en => wr_en2, data_in => data_in, data_out => data2);
+        reg3 : reg16bits PORT MAP(clk => clk, reset => reset, wr_en => wr_en3, data_in => data_in, data_out => data3);
+        reg4 : reg16bits PORT MAP(clk => clk, reset => reset, wr_en => wr_en4, data_in => data_in, data_out => data4);
 
-        reset_global : process
-        begin
-            reset <= '1';
-            wait for period*2;
-            reset <= '0';
-            wait;
-        end process reset_global;
+        wr_en0 <= '1' when wr_en = '1' and reg_wr = "000" else '0';
+        wr_en1 <= '1' when wr_en = '1' and reg_wr = "001" else '0';
+        wr_en2 <= '1' when wr_en = '1' and reg_wr = "010" else '0';
+        wr_en3 <= '1' when wr_en = '1' and reg_wr = "011" else '0';
+        wr_en4 <= '1' when wr_en = '1' and reg_wr = "100" else '0';
 
-        sim_time_proc : process
-        begin
-            wait for 10 us;
-            finished <= '1';
-            wait;
-        end process sim_time_proc;
+        data_out <= data0 when reg_read = "000" else
+                    data1 when reg_read = "001" else
+                    data2 when reg_read = "010" else
+                    data3 when reg_read = "011" else
+                    data4 when reg_read = "100" else
+                    "0000000000000000";
 
-        clock_proc: process
-        begin
-            while finished /= '1' loop
-                clk <= '0';
-                wait for period / 2;
-                clk <= '1';
-                wait for period / 2;
-            end loop;
-            wait;
-        end process clock_proc;
-
-        process
-            begin 
-                wait for 200 ns;
-                wr_en <= '0';
-                data_in <= "1111111111111111";
-                wait for 100 ns;
-                wr_en <= '1';
-                data_in <= "1111111111111111";
-                wait for 200 ns;
-                wr_en <= '1';
-                data_in <= "1100110010000000";
-                wait for 200 ns;
-                wr_en <= '0';
-                data_in <= "0000000000000000";
-                wait for 200 ns;
-                wait;
-        end process;
 end architecture;
